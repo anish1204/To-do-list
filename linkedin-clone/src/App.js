a6cfb97bb28604be67486552ea660adb0d16d64c
Anish Patil <anishpatil304@gmail.com>
2022-05-11 12:56:21 +0530

    Test1

---
+++ linkedin-clone/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter,Router, Route, Switch } from "react-router-dom";
+import './App.css';
+import Login from './components/Login';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login></Login>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
